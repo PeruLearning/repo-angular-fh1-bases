55eabb73429abc6799c1d287236b92768fcc69b4
Edinson Aldaz <edinson.aldaz@gmail.com>
2024-06-02 17:17:21 -0500

    Listado de Heroes

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
+  public name: string = 'Ironman';
+  public age: number = 45;
 
+  public changeName(): void {
+    this.name = 'Hulk';
+  }
+
+  public changeAge(): void {
+    this.age = 48;
+  }
+
+  public get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  public get heroDescription(): string {
+    return `${this.name} - ${this.age}`;
+  }
 }
