85d630c78775e107d974f5dced0608858b2f8cc6
Edinson Aldaz <edinson.aldaz@gmail.com>
2024-06-03 05:55:37 -0500

    Create add-character component

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { DbzListComponent } from './components/list/list.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
 
 
 
 @NgModule({
   declarations: [
     MainPageComponent,
-    DbzListComponent
+    DbzListComponent,
+    AddCharacterComponent
   ],
   imports: [
     CommonModule
