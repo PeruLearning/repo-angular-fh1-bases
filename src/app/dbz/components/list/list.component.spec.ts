f76137e731bcb2c7d6c7cfb091d88896366ba864
Edinson Aldaz <edinson.aldaz@gmail.com>
2024-06-03 06:38:49 -0500

    Paso de parámetros de un componente padre a un componente hijo

--- src/app/dbz/components/list/list.component.spec.ts
+++ src/app/dbz/components/list/list.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ListComponent } from './list.component';
+import { DbzListComponent } from './list.component';
 
 describe('ListComponent', () => {
-  let component: ListComponent;
-  let fixture: ComponentFixture<ListComponent>;
+  let component: DbzListComponent;
+  let fixture: ComponentFixture<DbzListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ListComponent]
+      declarations: [DbzListComponent]
     })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(ListComponent);
+      .compileComponents();
+
+    fixture = TestBed.createComponent(DbzListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
