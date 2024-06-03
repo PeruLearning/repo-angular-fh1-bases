c67822cc861fe5dc80291d0764da0a4e7660bd57
Edinson Aldaz <edinson.aldaz@gmail.com>
2024-06-03 05:53:39 -0500

    Modulo Db List

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { DbzListComponent } from './components/list/list.component';
 
 
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    DbzListComponent
   ],
   imports: [
     CommonModule
