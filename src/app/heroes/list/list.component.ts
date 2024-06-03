347525676905bcb89613849e8a72fb130b6b2f82
Edinson Aldaz <edinson.aldaz@gmail.com>
2024-06-02 19:17:19 -0500

    directiva ngIf

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class HeroesListComponent {
 
+  public heroNames: string[] = ['Spiderman', 'Ironman', 'Hulk', 'She Hulk', 'Thor'];
+  public deletedHero?: string = undefined;
+
+  public removeLastHero(): void {
+    this.deletedHero = this.heroNames.pop();
+  }
 }
