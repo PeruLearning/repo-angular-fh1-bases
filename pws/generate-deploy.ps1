76e67718e67aff3e468a862da2b8635a984f295e
Edinson Aldaz <edinson.aldaz@gmail.com>
2024-06-04 09:52:55 -0500

    Ajuste en el Script de Powershell

--- pws/generate-deploy.ps1
+++ pws/generate-deploy.ps1
@@ -2,7 +2,7 @@ ng build --base-href  ./
 
 # Ruta de origen y destino
 $source = "./dist/02-bases/browser"
-$destination = "./docs/"
+$destination = "./docs"
 
 # Verificar si el directorio de destino existe, si no, crearlo
 if (-Not (Test-Path -Path $destination)) {
