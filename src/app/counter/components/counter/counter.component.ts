9115c69474e8d934076f48665f6fa942a0b8301f
Edinson Aldaz <edinson.aldaz@gmail.com>
2024-06-23 15:06:11 -0500

    Ajustes

--- src/app/counter/components/counter/counter.component.ts
+++ src/app/counter/components/counter/counter.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-counter',
+  selector: 'counter-counter',
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css'
 })
